2672332592b9bd618b2ac0a2f1b6cb2215b78837
Neelesh99 <neelesh.ravichandran1999@gmail.com>
2021-02-05 12:26:12 +0000

    Application model created

--- todo/tasks/models.py
+++ todo/tasks/models.py
@@ -9,3 +9,14 @@ class Task(models.Model):
     def __str__(self):
         return self.title
 
+class Application(models.Model):
+    name = models.CharField(max_length=200)
+    open_time = models.DateTimeField()
+    close_time = models.DateTimeField()
+    open = models.BooleanField(default=False)
+    url = models.CharField(max_length=1000)
+    cv_uploaded = models.BooleanField(default=False)
+    cover_letter_uploaded = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
